1b0faa9b6b95cb6633f4295df8d74b762bcc96bc
RockNimbus <ec@rockecom.net>
2025-05-28 07:47:17 +0100

    added cors

--- Cloud/device-comms/src/main.rs
+++ Cloud/device-comms/src/main.rs
@@ -8,20 +8,27 @@ use dotenvy::dotenv;
 use rocket::{
     routes,
 };
+use rocket_cors::{AllowedOrigins, CorsOptions};
 
 #[get("/")]
 fn hello() -> &'static str {
     "Hello, world!"
 }
 
+
 #[rocket::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
-
+    let cors = CorsOptions {
+        allowed_origins: AllowedOrigins::All,
+        ..Default::default()
+    }.to_cors()?;
+    
     let rocket = rocket::build()
         .configure(rocket::Config::figment()
             .merge(("secret_key", std::env::var("SECRET_KEY").unwrap()))
             .merge(("address", "0.0.0.0")))
+            .attach(cors)
         .mount("/", routes![hello])
         .mount("/iot/data", routes![ingest_telemetry::ingest]);
 
