020b8fd8292010dfc12e23d5e719a3a049f0cb77
RockNimbus <ec@rockecom.net>
2025-05-28 07:09:21 +0100

    Add dockerfile and update main.rs

--- Cloud/device-comms/src/main.rs
+++ Cloud/device-comms/src/main.rs
@@ -4,16 +4,28 @@ mod routes;
 
 use routes::ingest_telemetry;
 
+use dotenvy::dotenv;
 use rocket::{
     routes,
 };
 
+#[get("/")]
+fn hello() -> &'static str {
+    "Hello, world!"
+}
+
 #[rocket::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+
     let rocket = rocket::build()
+        .configure(rocket::Config::figment()
+            .merge(("secret_key", std::env::var("SECRET_KEY").unwrap()))
+            .merge(("address", "0.0.0.0")))
+        .mount("/", routes![hello])
         .mount("/iot/data", routes![ingest_telemetry::ingest]);
 
-    println!("listening on 127.0.0.1:8000");
+    println!("listening on 0.0.0.0:8000");
     rocket.launch().await?;
     Ok(())
 }
